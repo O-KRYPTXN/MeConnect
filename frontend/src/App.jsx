37f9378856eab3f65f3350fd32b0cd3f8ca3c978
Abdelrahman Ahmed Mohammed <148643895+Abdo-zero@users.noreply.github.com>
2025-05-10 12:26:51 +0300

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,38 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import SignUpPage from "./pages/auth/SignUpPage"; 
+import LoginPage from "./pages/auth/LoginPage";
+import toast, { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "./lib/axios";
+
+function App() {
+  const {data:authUser} = useQuery({
+    queryKey:['authUser'],
+    queryFn:async()=>{
+      try{
+        const res = await axiosInstance.get("/auth/me");
+        return res.data;
+      }catch(err){
+        if(err.response && err.response.status===401){
+          return null;
+        }
+        toast.error(err.response.data.message || "there is something wrong");
+      }
+    }
+  });
+  console.log("authuser",authUser);
+  return <Layout>
+    <Routes>
+      <Route 
+        path="/" 
+        element={authUser ? <HomePage /> : <Navigate to="/login" replace />} 
+      />     
+      <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />} />
+      <Route path='/login' element={!authUser ?<LoginPage />: <Navigate to={"/"} />} />
+    </Routes>
+    <Toaster />
+  </Layout>;
+}
+
+export default App;
