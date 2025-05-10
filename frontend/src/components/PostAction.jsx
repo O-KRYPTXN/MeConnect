30af18e1a6d4a31cff4f7de8ce958e366e833e06
Abdelrahman Ahmed Mohammed <148643895+Abdo-zero@users.noreply.github.com>
2025-05-10 11:49:18 +0300

    Add files via upload

---
+++ frontend/src/components/PostAction.jsx
@@ -0,0 +1,8 @@
+export default function({icon,text,onClick}){
+    return(
+        <button className="flex items-center" onClick={onClick}>
+            <span className="mr-1">{icon}</span>
+            <span className="hidden sm:inline">{text}</span>
+        </button>
+    )
+}
